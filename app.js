b60e784a56f928fe4e4e22c6730bd739c1ba0faa
Gurvinder Singh <66419304+Gurvinder-Singh02@users.noreply.github.com>
2023-08-15 06:12:18 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+btn = document.querySelector("button");
+
+let output_Month = document.querySelector("#age_Months")
+let output_year = document.querySelector("#age_Years")
+let output_Day = document.querySelector("#age_Days")
+
+
+btn.addEventListener("click", () => {
+    let date = new Date;
+    console.log(date.getMonth());
+
+    let input_year = document.querySelector("#year").value;
+    let input_month = document.querySelector("#month").value;
+    let input_day = document.querySelector("#day").value;
+
+    let dob_str = `${input_year}-${input_month}-${input_day}`
+
+    const dob = new Date(dob_str);
+    const now = new Date();
+
+    let total_days = Math.floor((now - dob) / (1000 * 60 * 60 * 24));
+
+
+    let years = date3.getFullYear() - date2.getFullYear();
+    let months = date3.getMonth() - date2.getMonth();
+    let days = date3.getDate() - date2.getDate();
+
+
+    if (months < 0) {
+        output_year.innerText = Math.abs(years);
+    }else{   
+        output_Month.innerText = months;
+    }
+    output_year.innerText = years;
+    output_Day.innerText = days;
+})
